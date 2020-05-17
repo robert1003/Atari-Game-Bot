4b5b9aaaaf186f4f8d2e52eef11c600f4c931de9
robert1003 <b07902047@ntu.edu.tw>
2020-05-17 15:00:49 +0800

    upd pic

--- p2.py
+++ p2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-fig, axs = plt.subplots(1, 2, figsize=(20, 7))
-for i, gamma in enumerate([0.1, 0.3, 0.5, 0.7, 0.9]):
+fig, axs = plt.subplots(1, 1, figsize=(10, 7))
+for i, gamma in enumerate([0.1, 0.3, 0.5, 0.7, 0.9, 0.99]):
     epochs = []
     rewards = []
     with open('dqn_gamma_{}.log'.format(gamma), 'r') as f:
@@ -13,11 +13,11 @@ for i, gamma in enumerate([0.1, 0.3, 0.5, 0.7, 0.9]):
             rewards.append(reward)
        
     print(len(epochs), len(rewards))
-    axs[0].plot(epochs[:3500:50], rewards[:3500:50], label='{}'.format(gamma))
-    axs[0].set_xlabel('Episode')
-    axs[0].set_ylabel('Rewards')
-    axs[0].legend()
-
+    axs.plot(epochs[:3500:25], rewards[:3500:25], label='{}'.format(gamma))
+    axs.set_xlabel('Episode')
+    axs.set_ylabel('Rewards')
+    axs.legend()
+'''
 for i, gamma in enumerate([0.9, 0.95, 0.96, 0.97, 0.98, 0.99]):
     epochs = []
     rewards = []
@@ -34,5 +34,6 @@ for i, gamma in enumerate([0.9, 0.95, 0.96, 0.97, 0.98, 0.99]):
     axs[1].set_xlabel('Episode')
     axs[1].set_ylabel('Rewards')
     axs[1].legend()
+'''
 plt.savefig('p2.jpg', bbox_inches='tight')
 #plt.show()
