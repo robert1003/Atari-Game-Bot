ac96084923ade04c1fb8239c4baa16c1ba98cbd2
robert1003 <b07902047@ntu.edu.tw>
2020-05-18 09:51:27 +0800

    upd

--- p3_pg.py
+++ p3_pg.py
@@ -11,10 +11,17 @@ for name in ['pg.log', 'pg_vr.log']:
             epochs.append(epoch)
             rewards.append(reward)
    
+    a1, a2 = [], []
+    for _ in range(len(epochs) - 200):
+        a1.append((_ + 1) * 10)
+        a2.append(sum(rewards[_:_+100]) / 100)
+
     if name == 'pg.log':
-        plt.plot(epochs[:4000:10], rewards[:4000:10], label='vanilla')
+        #plt.plot(epochs[:4000:10], rewards[:4000:10], label='vanilla')
+        plt.plot(a1[:4000], a2[:4000], label='vanilla')
     else:
-        plt.plot(epochs[:4000:10], rewards[:4000:10], label='variance reduction')
+        #plt.plot(epochs[:4000:10], rewards[:4000:10], label='variance reduction')
+        plt.plot(a1[:4000], a2[:4000], label='variance reduction')
 plt.title('LunarLander-v2')
 plt.xlabel('Epochs')
 plt.ylabel('Rewards')
