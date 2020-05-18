392753e8abf43e70404a8006901f8dc9d1ee7034
robert1003 <b07902047@ntu.edu.tw>
2020-05-18 09:56:02 +0800

    upd

--- p3_dqn.py
+++ p3_dqn.py
@@ -21,15 +21,22 @@ for name in ['dqn_gamma_boxing_0.99.log', 'dqn_gamma_DDQN_boxing_0.99.log']:
         mean = su / (i + 1)
     print(ci)
     '''
+    a1, a2 = [], []
+    for _ in range(len(epochs) - 100):
+        a1.append((_ + 1) * 10)
+        a2.append(sum(rewards[_:_+100]) / 100)
+    print(len(a1), len(a2))
     if 'DDQN' in name:
         '''
         for i, re in enumerate(rewards):
             if i > 0:
                 rewards[i] = np.clip(rewards[i], rewards[i - 1] - 8, rewards[i - 1] + 8)
         '''
-        plt.plot(epochs[:566:10], rewards[:566:10], label='DDQN')
+        #plt.plot(epochs[:566:10], rewards[:566:10], label='DDQN')
+        plt.plot(a1[:460], a2[:460], label='DDQN')
     else:
-        plt.plot(epochs[:566:10], rewards[:566:10], label='DQN')
+        #plt.plot(epochs[:566:10], rewards[:566:10], label='DQN')
+        plt.plot(a1[:460], a2[:460], label='DQN')
 plt.legend()
 plt.title('Boxing-v0')
 plt.xlabel('Epochs')
