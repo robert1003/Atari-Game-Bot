b1527f1e20072631c13edf9573942c303dda5cce
robert1003 <b07902047@ntu.edu.tw>
2020-05-18 09:48:09 +0800

    upd

--- p1_pg.py
+++ p1_pg.py
@@ -10,9 +10,13 @@ with open('pg.log', 'r') as f:
         epochs.append(epoch)
         rewards.append(reward)
    
+a1, a2 = [], []
+for _ in range(len(epochs) - 100):
+    a1.append((_ + 1) * 10)
+    a2.append(sum(rewards[_:_+100]) / 100)
 plt.title('Policy Gradients')
 plt.xlabel('Epochs')
 plt.ylabel('Rewards')
-plt.plot(epochs, rewards)
+plt.plot(a1, a2)
 plt.savefig('p1_pg.jpg')
 #plt.show()
